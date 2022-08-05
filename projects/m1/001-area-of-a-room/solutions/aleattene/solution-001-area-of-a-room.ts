197f0a7926b8d8e6b29d6cda0e2ee4439ee0f43d
aleattene <attene.tomorrowdevs@gmail.com>
2022-08-05 18:21:02 +0200

    ci(tests): added tests exe 001

--- projects/m1/001-area-of-a-room/solutions/aleattene/solution-001-area-of-a-room.ts
+++ projects/m1/001-area-of-a-room/solutions/aleattene/solution-001-area-of-a-room.ts
@@ -1,13 +1,15 @@
-const input = require('prompt-sync')();
+//const input = require('prompt-sync')();
 
-let widthRoom: number = Number(input('Enter the width of the room (meters): '));
-let lengthRoom: number = Number(input('Enter the length of the room (meters): '));
-
-const areaOfRoom: number = (widthRoom * lengthRoom);
-
-if (isNaN(widthRoom) || isNaN(lengthRoom) || (!widthRoom) || (!lengthRoom)) {
-    console.log(`I'm sorry. You have entered invalid data.`);
-} else {
-    console.log(`Area of the room: ${areaOfRoom} square meters.`);
+export const calculateAreaRoom = (widthRoom: number, lengthRoom: number): number => {
+    return (widthRoom * lengthRoom);
 }
 
+// let widthRoom: number = input('Enter the width of the room (meters): ');
+// let lengthRoom: number = input('Enter the length of the room (meters): ');
+//
+// if (isNaN(widthRoom) || isNaN(lengthRoom) || (!widthRoom) || (!lengthRoom)) {
+//     console.log(`I'm sorry. You have entered invalid data.`);
+// } else {
+//     const areaOfRoom: number = calculateAreaRoom(widthRoom, lengthRoom)
+//     console.log(`Area of the room: ${areaOfRoom} square meters.`);
+// }
---
+++ projects/m1/001-area-of-a-room/tests/aleattene/solution-001-area-of-a-room.test.ts
@@ -0,0 +1,9 @@
+
+import { calculateAreaRoom } from '../../solutions/aleattene/solution-001-area-of-a-room';
+
+describe('CalculateAreaRoom', () => {
+    it('Test', () => {
+        expect(calculateAreaRoom(100, 100)).toBe(10000);
+
+    });
+});
