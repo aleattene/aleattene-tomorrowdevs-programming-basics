e26890ca5fca1ec0ba4bb1a86c0930efeb7bc407
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-08-06 15:23:25 +0200

    feat(m1): added TS solution and tests - exercise 003-making-change (#4)

---
+++ projects/m1/003-making-change/tests/aleattene/solution-003-making-change.test.ts
@@ -0,0 +1,26 @@
+
+import { changeMoney } from '../../solutions/aleattene/solution-003-making-change';
+
+describe('calculateChange', () => {
+    it('Test', () => {
+        expect(changeMoney(200)).toEqual(
+            { toonies: 1, loonies: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        );
+        expect(changeMoney(100)).toEqual(
+            { toonies: 0, loonies: 1, quarters: 0, dimes: 0, nickels: 0, pennies: 0 }
+        );
+        expect(changeMoney(25)).toEqual(
+            { toonies: 0, loonies: 0, quarters: 1, dimes: 0, nickels: 0, pennies: 0 }
+        );
+        expect(changeMoney(10)).toEqual(
+            { toonies: 0, loonies: 0, quarters: 0, dimes: 1, nickels: 0, pennies: 0 }
+        );
+        expect(changeMoney(5)).toEqual(
+            { toonies: 0, loonies: 0, quarters: 0, dimes: 0, nickels: 1, pennies: 0 }
+        );
+        expect(changeMoney(1)).toEqual(
+            { toonies: 0, loonies: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 1 }
+        );
+
+    });
+});
