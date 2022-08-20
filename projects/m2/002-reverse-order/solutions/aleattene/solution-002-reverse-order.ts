bee6a47fef55ae2d16204e175589efa6dc4c24e1
aleattene <attene.tomorrowdevs@gmail.com>
2022-08-20 15:38:00 +0200

    feat(m2): added TS solution exercise 002-reversed-order

---
+++ projects/m2/002-reverse-order/solutions/aleattene/solution-002-reverse-order.ts
@@ -0,0 +1,15 @@
+import {dataInput} from "../../../../utils/dataInput";
+
+const integers: number[] = [];
+let input = true;
+
+while (input) {
+    let integerEntered = dataInput("Enter an integer number (zero to quit): ", "Integer");
+    if (integerEntered === '0') input = false;
+    else integers.push(parseInt(integerEntered));
+}
+
+integers.reverse();
+
+if (integers.length !== 0) integers.forEach((elem) => console.log(elem));
+else console.log('No Integer entered.');
